790f8006f7ee35c73017fd5d50f53c383927625a
marcin1989 <marcinlubanski23@gmail.com>
2022-02-11 09:30:54 +0100

    43-js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+function matma (num1, num2) {
+    let sum = num1 + num2;
+    let substraction = num1 - num2;
+    let product = num1 * num2;
+   
+    
+    
+
+    if (sum > 0) {
+        console.log("Wynik dodawania wynosi " + sum)  
+    } else { 
+        console.log("Wynik nieprawidlowy " + sum)
+    }
+    if (substraction > 0) {
+        console.log("Wynik odejmowania wynosi "  + substraction)
+    } else {
+        console.log("Wynik nieprawidlowy " + substraction)
+    }
+    if (product > 0) {
+        console.log("Wynik mnozenia wynosi "  +  product)
+    } else {
+        console.log("Wynik nieprawidlowy " + product)
+    }
+    
+   }
+
+matma(-4,-3);
